ebc53f40130ad3fcafd3ead33d7cb0121695568b
Zibran Shaikh <zshaikh@isystango.com>
2019-11-12 15:20:35 +0530

    Fix: fix package json warnings.

--- src/actions/Auth/index.js
+++ src/actions/Auth/index.js
@@ -1,21 +1,8 @@
 import api from '../../service/api';
-import {
-  GET_LIST,
-  GET_LIST_SUCCESS,
-  GET_LIST_FAILURE
-} from './actionTypes'
+import { GET_LIST } from './actionTypes'
 
 export const getList = () => ({
   type: GET_LIST,
   payload: api.get('/asia'),
 });
 
-
-// export const getListSuccess = (data) => ({
-//   type: GET_LIST_SUCCESS,
-//   payload: data
-// })
-
-// export const getListFailure = () => ({
-//   type: GET_LIST_FAILURE
-// })
